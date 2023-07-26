22c402d535f96df9396a24746f6530337259aae5
rigobersio <rigomartinez@udec.cl>
2023-07-26 19:43:39 -0400

    (mxnote)__pholderswitchYJSswitch_()

---
+++ switch/10-examples.js
@@ -0,0 +1,67 @@
+
+// 1) Escribe un programa que tome un número del 1 al 7 como entrada
+//y muestre el día de la semana correspondiente (1 para "Lunes", 2 para "Martes", etc.).
+
+var arr = ['ERROR', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+
+funtion(num){
+	switch(num){
+	case 1:
+		return arr[num];
+	break;
+	case 2
+		return arr[num];
+	break;
+	case 3
+		return arr[num];
+	break;
+	case 4
+		return arr[num];
+	break;
+	case 5
+		return arr[num];
+	break;
+	case 6
+		return arr[num];
+	break;
+	case 7
+		return arr[num];
+	break;
+	default
+		return arr[0];
+	break;
+}
+}
+
+
+/*
+
+    Crea un programa que solicite un mes del año (número del 1 al 12) y muestre el nombre del mes correspondiente.
+
+    Haz un programa que tome un carácter (letra) como entrada y determine si es una vocal o una consonante.
+
+    Escribe un programa que tome una letra del alfabeto y muestre si es una letra mayúscula o minúscula.
+
+    Crea un programa que solicite un número del 1 al 12 y muestre la cantidad de días que tiene el mes correspondiente (ignora años bisiestos).
+
+    Haz un programa que solicite una dirección cardinal (Norte, Sur, Este, Oeste) y muestre un mensaje específico para cada dirección.
+
+    Escribe un programa que tome una puntuación numérica (del 0 al 100) y muestre el grado equivalente (A, B, C, D o F) según la escala de calificación.
+
+    Crea un programa que solicite una operación matemática (+, -, *, /) y realice la operación entre dos números ingresados por el usuario.
+
+    Haz un programa que tome una moneda (por ejemplo, "dólar" o "euro") y convierta una cantidad en pesos colombianos a la moneda seleccionada.
+
+    Escribe un programa que tome un número del 1 al 5 y muestre un patrón diferente para cada número utilizando caracteres o símbolos.
+
+    Crea un programa que solicite un código de país (por ejemplo, "USA" o "ESP") y muestre el idioma oficial correspondiente.
+
+    Haz un programa que tome una calificación (A, B, C, D o F) y muestre un mensaje personalizado según la calificación obtenida.
+
+    Escribe un programa que solicite una tarjeta de crédito y muestre el tipo de tarjeta (Visa, Mastercard, American Express, etc.) basado en el número ingresado.
+
+    Crea un programa que simule un menú de opciones (por ejemplo, "1. Ver información", "2. Agregar dato", etc.) y realice la acción seleccionada por el usuario.
+
+    Haz un programa que tome una dirección IP y determine si es una dirección IPv4 o IPv6.
+
+    */
